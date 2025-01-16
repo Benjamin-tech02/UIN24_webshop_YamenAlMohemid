60423928826fdf3d638245e83ef98396c64b7e3f
Benjamin-tech02 <yamena@hiof.no>
2025-01-16 17:36:53 +0100

    .

--- Legodudes/shopscript.js
+++ Legodudes/shopscript.js
@@ -1,4 +1,4 @@
-console.log(products)
+console.log(products);
 // gffugkjhkjnnkjjgygykg7gihuiuuhihihhhhihuihlmøjøoøliuuigyugiytfyugu  hhguy hi //
 
 let productHTML = ""
@@ -9,8 +9,47 @@ products.map((product, index) => productHTML += `
                 <a href="#KATEGORISIDE">Ninjago</a>
                 <h3>${product.title}</h3>
                 <p>Kr.${product.price},-</p>
-                <button>Legg i handlekurv</button>
-            </article>` )
+                <button onclick = "addProductToCart(${product.prodid})">Legg i handlekurv</button>
+            </article>` );
 
-document.getElementById("productlist").innerHTML = productHTML
+document.getElementById("productlist").innerHTML = productHTML;
+document.getElementById("shoppingcart").addEventListener("click", function() {
+    document.getElementById("cart").classList.toggle("visible");
+})
+
+function addProductToCart(prodid) {
+    console.log("Du vil legge til produktid" + prodid);
+
+    const idExist = cart.some(cartprod => cartprod.prodid === prodid);
+    if (idExist){
+        const index = cart.findIndex(p => p.cartProdid = prodid);
+        cart[index].quantity++;
+    } 
+    else {
+        cart.push({cartProdid: prodid, quantity: 1});
+    }
+
+    console.log(cart);
+}
+
+function printCart(){
+    let cartHTML ="";
+    cart.map((cartprod, index) => {
+        const currentProduct = products.findIndex(p => p.prodid === cartprod.cartProdid);
+        const currentProductInfo = products[currentProduct];
+        cartHTML += `<article class="cart-product">
+                    <span class="title">${currentProductInfo.title}</span>
+                    <span class="price">${currentProductInfo.price},-</span>
+                    <span class="quantity">x<span class="quantity-number">${cartprod.quantity}</span></span>
+                    <button class="delete">x</button>
+                </article>`;
+        
+        cartTotal += currentProductInfo.price * cartprod.quantity;
+        cartNumber += cartprod.quantity; 
+    })
+
+    document.getElementById("cart-products").innerHTML = cartHTML;
+    document.getElementById("cart-total").innerHTML = cartTotal;
+    document.getElementById("cartcount").innerHTML = cartNumber;
+}
 
